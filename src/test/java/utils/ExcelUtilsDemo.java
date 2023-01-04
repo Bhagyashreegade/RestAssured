0b8636f5ee8a5a45eefbec7aff971b5d3b436727
Bhagyashree Gade <bhagyashreegade91@gmail.com>
2023-01-03 21:11:59 -0500

    read data from excel

---
+++ src/test/java/utils/ExcelUtilsDemo.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class ExcelUtilsDemo {
+
+    public static void main(String[] args) {
+
+        String excelPath = "./data/TestDataRest.xlsx";
+        String sheetName = "Sheet1";
+
+        ExcelUtils excel = new ExcelUtils(excelPath, sheetName);
+        excel.getRowCount();
+        excel.getCellData(1, 1);
+    }
+
+
+}
